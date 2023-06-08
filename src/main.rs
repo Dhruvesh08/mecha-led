6d47d8bcc0a6568282ea2cb6991460e0a78b7565
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-08 16:48:12 +0530

    red led control for mecha

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Write};
+use std::process::Command;
+
+fn execute_command(command: &str) -> Result<(), std::io::Error> {
+    let output = Command::new("/bin/sh")
+        .arg("-c")
+        .arg(command)
+        .output()?;
+
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!(
+                "Command '{}' failed with exit code {:?}",
+                command, output.status.code()
+            ),
+        ))
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let mut input = String::new();
+
+    print!("Enter a value for LED control: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut input)?;
+
+    match input.trim() {
+        "on" => execute_command("echo 1 | sys/class/leds/red-led/brightness")?,
+        "off" => execute_command("echo 0 |  sys/class/leds/red-led/brightness")?,
+        "heartbeat" => execute_command("echo heartbeat | sudo tee /sys/class/leds/sys_led/trigger")?,
+        _ => println!("Invalid input!"),
+    }
+
+    Ok(())
+}
