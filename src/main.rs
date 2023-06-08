0161c17315914af970c050ca80911dec5b101b18
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-08 17:07:05 +0530

    all led control for mecha

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,29 @@ fn execute_command(command: &str) -> Result<(), std::io::Error> {
 fn main() -> Result<(), std::io::Error> {
     let mut input = String::new();
 
-    print!("Enter a value for LED control: ");
+    print!("Enter an LED name (red/green/blue): ");
     io::stdout().flush()?;
     io::stdin().read_line(&mut input)?;
-  
+
+    let led_name = match input.trim() {
+        "red" => "/sys/class/leds/red-led/brightness",
+        "green" => "/sys/class/leds/green-led/brightness",
+        "blue" => "/sys/class/leds/blue-led/brightness",
+        _ => {
+            println!("Invalid LED name!");
+            return Ok(());
+        }
+    };
+
+    print!("Enter an LED state (on/off): ");
+    io::stdout().flush()?;
+    input.clear();
+    io::stdin().read_line(&mut input)?;
+
     match input.trim() {
-        "on" => execute_command("echo 1 > /sys/class/leds/red-led/brightness")?,
-        "off" => execute_command("echo 0 > /sys/class/leds/red-led/brightness")?,
-        "heartbeat" => execute_command("echo heartbeat | tee /sys/class/leds/sys_led/trigger")?,
-        _ => println!("Invalid input!"),
+        "on" => execute_command(&format!("echo 1 > {}", led_name))?,
+        "off" => execute_command(&format!("echo 0 > {}", led_name))?,
+        _ => println!("Invalid LED state!"),
     }
 
     Ok(())
